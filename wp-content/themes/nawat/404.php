42667b7fbe72615092749a995338f98f6be181bc
Youssef Mohamed <hendaoye.201250@gamil.com>
2025-06-15 13:55:46 +0300

    feat : 404 page update

--- wp-content/themes/nawat/404.php
+++ wp-content/themes/nawat/404.php
@@ -26,18 +26,33 @@ if (isset($block)) {
 ?>
 <div class="page-not-found">
   <div class="container">
-    <div class="content-wrapper ">
-          <h1  class="main-title  navy-color text-center">
-            404
-          </h1>
-          <div class="description body navy-color text-center nawat-h2">
-            Sorry, the page you are looking for could not be found. It might have been removed, renamed, or did not exist in the first place.
-          </div>
-        <a href="<?php site_url(); ?>"
-           class="cta-button main-cta-button has-icon cta-button" >
-          Back to Home
-        </a>
-    </div>
+      <div class="content-wrapper">
+          <?php
+          $lang = function_exists('pll_current_language') ? pll_current_language() : get_locale();
+          ?>
+
+          <?php if ($lang === 'ar' || $lang === 'ar_EG'): ?>
+              <h1 class="main-title navy-color text-center">
+                  404
+              </h1>
+              <div class="description body navy-color text-center nawat-h2">
+                  عذرًا، الصفحة التي تبحث عنها غير موجودة. ربما تم حذفها، أو تغيير اسمها، أو لم تكن موجودة من الأساس.
+              </div>
+              <a href="<?php echo site_url(); ?>" class="cta-button main-cta-button has-icon cta-button">
+                  العودة إلى الصفحة الرئيسية
+              </a>
+          <?php else: ?>
+              <h1 class="main-title navy-color text-center">
+                  404
+              </h1>
+              <div class="description body navy-color text-center nawat-h2">
+                  Sorry, the page you are looking for could not be found. It might have been removed, renamed, or did not exist in the first place.
+              </div>
+              <a href="<?php echo site_url(); ?>" class="cta-button main-cta-button has-icon cta-button">
+                  Back to Home
+              </a>
+          <?php endif; ?>
+      </div>
   </div>
 </div>
 <?php get_footer(); ?>
